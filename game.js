f0832751bcf593d95b96b1580ac4ae8a64a25747
Ggpro47 <matteo.zanon@student.h-is.com>
2022-05-25 09:25:28 +0200

    gameee

--- game.js
+++ game.js
@@ -15,7 +15,7 @@ document.onkeydown = (e) => {
     console.log('Keyboard pressed',e.code)   
 
     // if user click spacebar ....
-    if (e.code == 'ArrowUp' && jumping === false){
+    if (e.code == 'Space' && jumping === false){
         // add the class jump to sonic
         jumping = true
         console.log('Sonic jumping',jumping)
@@ -43,3 +43,54 @@ document.onkeydown = (e) => {
     }
 
 }
+
+
+// Managing the drop of an obstacle
+let dropping1 = false
+let droppingEl1 = document.querySelector('.obstacle')
+setInterval( () => {
+    
+    if (Math.random() < 0.4 && dropping1 === false){
+
+        // set random left
+        droppingEl1.style.left = parseInt(Math.random() * 100) + "%"
+
+        dropping1 =true
+        droppingEl1.classList.add('dropping')
+        setTimeout(() => {
+            dropping1 =false
+            droppingEl1.classList.remove('dropping')
+        },4000)
+    }
+
+},500)
+
+
+// collision detection
+setInterval(() => {
+    if (isCollide(sonic, droppingEl1)){
+        alert("you die")
+        location.href="index.html"
+    }
+
+    // 
+},30) 
+
+
+function isCollide(a, b) {
+    var aRect = a.getBoundingClientRect();
+    var bRect = b.getBoundingClientRect();
+
+    return !(
+        ((aRect.top + aRect.height) < (bRect.top)) ||
+        (aRect.top > (bRect.top + bRect.height)) ||
+        ((aRect.left + aRect.width) < bRect.left) ||
+        (aRect.left > (bRect.left + bRect.width))
+    );
+}
+
+
+setTimeout(() => {
+    alert("you won")
+    location.href="game2.html"
+},4*1000)
